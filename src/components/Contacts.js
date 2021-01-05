5ecb01219cf5d3bb79029ceececcfa5216410486
Andrew G. Saushkin <asaushkin@gmail.com>
2021-01-05 16:50:07 +0300

    Move state from constructor

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,30 +2,27 @@ import React, {Component} from 'react';
 import Contact from "./Contact";
 
 class Contacts extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            contacts: [
-                {
-                    id: 1,
-                    name: "John Doe",
-                    email: "jdoe@gmail.com",
-                    phone: "555-555-5555"
-                },
-                {
-                    id: 2,
-                    name: "Karen",
-                    email: "karen@gmail.com",
-                    phone: "555-555-11111"
-                },
-                {
-                    id: 3,
-                    name: "Henry",
-                    email: "henry@gmail.com",
-                    phone: "555-555-2222"
-                }
-            ]
-        }
+    state = {
+        contacts: [
+            {
+                id: 1,
+                name: "John Doe",
+                email: "jdoe@gmail.com",
+                phone: "555-555-5555"
+            },
+            {
+                id: 2,
+                name: "Karen",
+                email: "karen@gmail.com",
+                phone: "555-555-11111"
+            },
+            {
+                id: 3,
+                name: "Henry",
+                email: "henry@gmail.com",
+                phone: "555-555-2222"
+            }
+        ]
     }
 
     render() {
